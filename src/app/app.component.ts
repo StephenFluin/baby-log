233ca58357f142135d2ee14d0127597457379fb0
Stephen Fluin <stephen.fluin@gmail.com>
2021-03-24 06:47:02 -0700

    add analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 import { Auth } from './auth.service';
 import { UserData } from './user-data.service';
-import { NgxMaterialTimepickerTheme } from 'ngx-material-timepicker';
-import { Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+declare var ga: any;
+
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
 })
 export class AppComponent {
-    constructor(public auth: Auth, public userData: UserData, public router: Router) {}
+    constructor(public auth: Auth, public userData: UserData, public router: Router, public activatedRoute: ActivatedRoute) {
+        router.events.pipe(filter(e => e instanceof NavigationEnd)).subscribe((n: NavigationEnd) => {
+            const pageTitle = router.routerState.snapshot.root.children[0].data['title'];
+            ga('integration.send','pageview',n.urlAfterRedirects);
+        });   
+    }
     switchTo(child: {id:string,name:string}) {
         // Use a URL instead of switching directly
         this.router.navigate(['child',child.id,child.name]);
     }
+
+    
 }
